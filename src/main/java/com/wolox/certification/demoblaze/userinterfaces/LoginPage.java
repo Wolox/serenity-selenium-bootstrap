ea918e46709bb37c60414eb075d8451915761996
Wendy Sierra <wendy.sierra@wolox.co>
2022-01-13 11:39:13 -0500

    corrections pullrequest

--- src/main/java/com/wolox/certification/demoblaze/userinterfaces/LoginPage.java
+++ src/main/java/com/wolox/certification/demoblaze/userinterfaces/LoginPage.java
@@ -5,9 +5,9 @@ import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 public class LoginPage {
-    public static final Target TAB_LOGIN = Target.the("tab para abrir el formulario del login").located(By.id("login2"));
-    public static final Target USERNAME_FIELD = Target.the("input escribir el nombre de usuario").located(By.id("loginusername"));
-    public static final Target PASSWORD_FIELD = Target.the("input escribir la contraseña").located(By.id("loginpassword"));
-    public static final Target BTN_LOGIN = Target.the("boton para iniciar session").located(By.xpath("//button[text()='Log in']"));
-    public static final Target WELCOME_MESSAGE = Target.the("mensaje de bienvenida").located(By.id("nameofuser"));
+    public static final Target TAB_LOGIN = Target.the("tab to open the login form").located(By.id("login2"));
+    public static final Target USERNAME_FIELD = Target.the("input to enter username").located(By.id("loginusername"));
+    public static final Target PASSWORD_FIELD = Target.the("input to enter password").located(By.id("loginpassword"));
+    public static final Target BTN_LOGIN = Target.the("button to login").located(By.xpath("//button[text()='Log in']"));
+    public static final Target WELCOME_MESSAGE = Target.the("Welcome message").located(By.id("nameofuser"));
 }
